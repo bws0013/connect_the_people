b1fd4da724621067e71ea531e766f525c0866f95
Ben <i@auburn.edu>
2017-10-04 20:13:55 -0500

    adding gabs examples for testing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Mapping Relationships Between People
+
+ - Dependency alert: go get github.com/Jeffail/gabs
---
+++ src/examples/gabs/json_gabs_examples.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "fmt"
+  "github.com/Jeffail/gabs"
+
+)
+
+func main() {
+  find_kids()
+}
+
+// Bad method name
+func find_kids() {
+  jsonParsed, err := gabs.ParseJSON([]byte(`{"object":{ "first": 1, "second": 2, "third": 3 }}`))
+
+  if err != nil {
+    panic(err)
+  }
+
+  // S is shorthand for Search
+  children, _ := jsonParsed.S("object").ChildrenMap()
+  for key, child := range children {
+  	fmt.Printf("key: %v, value: %v\n", key, child.Data().(float64))
+  }
+}
