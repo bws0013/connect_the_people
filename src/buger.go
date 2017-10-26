991f9e38315938aa03167a0be2f636ef9a30b786
Ben <i@auburn.edu>
2017-10-25 22:16:28 -0500

    We can both add and change data, this is big

--- src/buger.go
+++ src/buger.go
@@ -8,19 +8,41 @@ import (
 )
 
 var (
+  polly = []byte(`{"person":{"name":"polly","traits":{"relative":{"brother":[{"uno":{"pet":[{"cat":"cat-do","kittens":[{"male":"one"},{"female":"two"}]},"dog","rat"]}}]}}}}`)
   edwin = []byte(`{"person":{"name":"edwin","traits":{"relative":{"brother":[{"uno":{"pet":[{"cat":"cat-do","kittens":[{"male":"one"},{"female":"two"}]},{"dog":"dog-do"},{"rat":"rat-do"}]}}]}}}}`)
-  path = "person.traits.relative.brother.[0].uno.pet.[0].kittens.[0].male"
+  path = "person.traits.relative.brother.[0].uno.pet.[1]"
 )
 
 func main() {
 
-  val, ty, _, err := jsonparser.Get(edwin, fix_path(path)...)
+  // create_this_person("joe")
+  // return
 
+  val, _, _, err := jsonparser.Get(polly, fix_path(path)...)
   check_err(err)
-
   fmt.Printf("%s\n", val)
-  fmt.Println(ty)
 
+
+  // val, _, _, err := jsonparser.Get(edwin, fix_path(path)...)
+  // check_err(err)
+  // fmt.Printf("%s\n", val)
+
+  // fmt.Println("================")
+  // fmt.Printf("%s\n", edwin)
+  // fmt.Println("================")
+  //
+  // new_val := []byte("\"dude\"")
+  //
+  // val, err = jsonparser.Set(edwin, new_val, fix_path(path)...)
+  // check_err(err)
+  // fmt.Printf("%s\n", val)
+
+}
+
+func create_this_person(name string) {
+  raw := fmt.Sprintf(`{"person":{"name":"%s"}}`, name)
+  this_person := []byte(raw)
+  fmt.Printf("%s\n", this_person)
 }
 
 func fix_path(path string) []string {
